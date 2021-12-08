d7a0cc9bbca321a8c857afe603751c78985993b5
Henrik Gjestang <henrik.logj@gmail.com>
2021-12-08 09:35:32 +0100

    added hello_world

--- da_vislib/da_vislib.py
+++ da_vislib/da_vislib.py
@@ -1,1 +1,18 @@
 """Main module."""
+
+
+def hello_world():
+    """Retun 'Hello World!'.
+
+    Returns
+    -------
+    str
+        "Hello World!"
+
+    Examples
+    --------
+    >>> x = hello_world()
+    >>> print (x)
+    Hello World!
+    """
+    return "Hello World!"
